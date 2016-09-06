10cc5a3fa6b96f6ae03c08f88dc4149b4fba9f08
Joseph E. Gonzalez <jegonzal@cs.berkeley.edu>
2016-09-05 23:59:08 -0700

    minor edits

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 
-JEKYLL_ENV=production bundle exec jekyll build 
+JEKYLL_ENV=production bundle exec jekyll build --baseurl 'http://people.eecs.berkeley.edu/~jegonzal' 
 rsync -avh _site/. login.eecs.berkeley.edu:~/public_html/.
